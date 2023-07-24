a0e6c6e7cd7ed9b61bc08d258fb3f48f6f8065e9
Ashutosh102 <ashutoshmohanty3815@gmail.com>
2023-07-24 20:10:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/steven-tey/novel",
+        permanent: true,
+      },
+      {
+        source: "/feedback",
+        destination: "https://github.com/steven-tey/novel/issues",
+        permanent: true,
+      },
+      {
+        source: "/deploy",
+        destination: "https://vercel.com/templates/next.js/novel",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
